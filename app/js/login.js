d38490f33ef5a23c505eca51ee40069411978eea
ydc <ydc201211@163.com>
2017-09-14 17:30:37 +0800

    add login page

---
+++ app/js/login.js
@@ -0,0 +1,24 @@
+(function(){
+   
+});
+function checkForm() {
+   
+    if(checkQcr){
+        validateUser()
+    }else{
+        return false;
+    }
+}
+function checkQCR(){
+    var res = verifyCode.validate(document.getElementById("code_input").value);
+    if(res){
+        return true;  
+    }else{
+        
+        $(".error").html("<p style='color:red;font-size:12px;'>验证码错误.</p>");
+        return false;
+    }
+}
+function validateUser(){
+
+}
